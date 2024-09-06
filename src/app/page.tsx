e149fb15f138fc0ea98652e3da0e9fae79c6f37d
João Gabriel <joao.gabsilva1007@gmail.com>
2024-09-06 10:27:18 -0300

    feat: [WIP] add logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import { auth } from "@/auth/auth";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   const { user } = await auth();
 
-  return <main>{JSON.stringify(user)}</main>;
+  return (
+    <main>
+      <div>{JSON.stringify(user)}</div>
+
+      <Button asChild>
+        <Link href="/api/auth/sign-out">Sign Out</Link>
+      </Button>
+    </main>
+  );
 }
